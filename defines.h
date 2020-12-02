0fe06a66000f70529c347fd5ecf4e4530e2fa3b1
Simon Olofsson <dotchetter@protonmail.ch>
2020-12-02 02:31:19 +0100

    Updated defines

--- defines.h
+++ defines.h
@@ -14,20 +14,13 @@
  * Written on 2020-11-26, Simon Olofsson
  */
 
-#define REF_VOLTAGE          3.3f
-#define ADC_MAX_VAL_REF      1024ul
-#define PH_REF_1             4.0f
-#define PH_REF_2             9.18
-#define PH_SENSOR_PIN        A1
-#define PH_SENSOR_CHANNEL    10u
-
 #define TEMP_PORT_GROUP      0
 #define TEMP_PINMASK         PORT_PA16
-#define WIFI_RECONNECT_WAIT  3000
 
-#define WIFI_SSID            ""
-#define WIFI_PASS            ""
-#define DEVICE_ID            ""
-#define STDBY_TIME_MS        2000
+#define SECOND               1000UL
+#define MINUTE               60 * SECOND
+
+#define RUN_TIME             15 * SECOND
+#define SLEEP_TIME           10 * MINUTE
 
 #endif // DEFINITIONS_H_
