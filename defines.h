60b9e67787773d2cbe3b96417182f03a2cd8615e
Simon Olofsson <dotchetter@protonmail.ch>
2021-01-14 22:33:11 +0100

    Whitespace added

--- defines.h
+++ defines.h
@@ -27,6 +27,7 @@
 #define DEVICE_ID            ""
 #define INTERVAL             30 * SECOND
 #define UPDATE_CYCLES        F_CPU / 1000000UL
+
 #define BATT_DELAY           1 * MINUTE
 #define BATT_MAX_V           3.950
 #define BATT_MIN_V           3.280
